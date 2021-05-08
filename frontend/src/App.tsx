2cd8e429c01e7982bd4eb8696fe119dcb2fc7dd1
John Travolta <m3u0v0@gmail.com>
2021-05-08 01:11:33 -0300

    Basic static components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,19 @@
+import Datatable from "components/DataTable";
+import Footer from "components/Footer";
+import NavBar from "components/NavBar";
+import React from "react";
+
 function App() {
   return (
-    <div>
-      <h1 className="text-primary">Hello world!</h1>
-    </div>
+    <>
+      <NavBar></NavBar>
+      <div className="container">
+        <h1 className="text-primary">Hello world!</h1>
+      </div>
+      <Datatable></Datatable>
+      <Footer></Footer>
+
+    </>
   );
 }
 
